4c460faeb7530e310813a9c137dc004b37491dab
Vivekachooz <vivekachooz@gmail.com>
2023-12-22 14:12:58 +0530

    kernel/sched: Revert previously applied sched changes
    Change-Id: Ie54c1b91bc7bb5031f3c28a29a7e217fd6219827

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -145,8 +145,8 @@ static int ten_thousand = 10000;
 static int six_hundred_forty_kb = 640 * 1024;
 #endif
 static int max_kswapd_threads = MAX_KSWAPD_THREADS;
-static int __maybe_unused two_hundred_million = 200000000;
 static int two_hundred_fifty_five = 255;
+static int __maybe_unused two_hundred_million = 200000000;
 
 #ifdef CONFIG_SCHED_WALT
 const int sched_user_hint_max = 1000;
@@ -784,7 +784,7 @@ static struct ctl_table kern_table[] = {
 		.data		= sched_lib_name,
 		.maxlen		= LIB_PATH_LENGTH,
 		.mode		= 0644,
-		.proc_handler	= sysctl_sched_lib_name_handler,
+		.proc_handler	= proc_dostring,
 	},
 	{
 		.procname	= "sched_lib_mask_force",
